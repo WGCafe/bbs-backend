9630d8cb39fd3704b283fb3c0e00e6a8106eb40f
wendylin <wendylw2013@gmail.com>
2019-03-31 18:58:24 +0800

    Login: Update logic

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
+var passport = require('passport');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var bodyParser = require('cookie-parser');
 var logger = require('morgan');
+var localStrategy = require('passport-local').Strategy;
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/server', {useNewUrlParser: true});
@@ -16,7 +18,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'lanya.bbs.me',
+  cookie: {
+    maxAge: 600000
+  }
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,6 +49,34 @@ app.use(function(err, req, res, next) {
   res.json({
     error: res.locals.error,
     message: err.message
+  });
+});
+
+passport.use('local', new localStrategy(
+  function (email, password, done) {
+      User.findOne({
+        email: 'admin',
+        password: 'pass'
+      }, function (err, user) {
+        if (email !== user.email) {
+            return done(null, false, { message: 'Incorrect email.' });
+        }
+        if (password !== user.password) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+      });
+  }
+));
+
+passport.serializeUser(function (user, done) {//保存user对象
+  done(null, user);//可以通过数据库方式操作
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var CatController = require('../controllers/CatController');
 var ArticleTypeController  = require('../controllers/ArticleTypeController');
+var ArticleController = require('../controllers/ArticleController');
 var UserController = require('../controllers/UserController');
 
 /* GET home page. */
@@ -12,10 +13,13 @@ router.get('/', function(req, res, next) {
 router.get('/api/cats', CatController.fetchAll);
 router.get('/api/articletype', ArticleTypeController.fetchAll);
 router.post('/api/articletypes-init', ArticleTypeController.init);
+
 router.post('/api/login', UserController.Login);
 router.post('/api/resetpassword', UserController.resetPassword);
 router.get('/api/users/:user_id', UserController.findOne);
 router.put('/api/users/:user_id', UserController.updateUser);
 router.post('/api/users', UserController.insert);
+
+router.post('/api/article', ArticleController.insert);
 
 module.exports = router;
